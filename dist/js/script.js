3862d0c3df70fecf22087ded1b15558aff17af95
Habstrakt <hunter3000@mail.ru>
2020-02-03 19:23:31 +1000

    add modal widow

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
   });
+
+  $('[data-modal=request]').on('click', function(){
+    $('.overlay, #request').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #request, #thanks').fadeOut('slow');
+  });
+  $('.button_question').on('click', function(){
+    $('.overlay, #thanks').fadeIn('slow');
+  })
 });
